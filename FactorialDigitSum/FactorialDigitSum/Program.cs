0fb9d8c0a3ee46a37b1b2fba1bd2c07510315b55
SIMRAN SINGH <SIMRAN514@GMAIL.COM>
2018-10-23 18:44:05 -0500

    added problem 21

--- FactorialDigitSum/FactorialDigitSum/Program.cs
+++ FactorialDigitSum/FactorialDigitSum/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace FactorialDigitSum
 {
@@ -10,9 +11,30 @@ namespace FactorialDigitSum
 	{
 		static void Main(string[] args)
 		{
+			BigInteger num = factorial(100);
+			long sum = 0;
 
+			string numString = Convert.ToString(num);
+			
+			for (int i = 0; i < numString.Length; i++)
+			{
+				sum += (long)Char.GetNumericValue(numString[i]);
+			}
+
+			Console.WriteLine(sum);
+			Console.ReadKey();
 		}
 
-		static long factorial()
+		static BigInteger factorial(int n)
+		{
+			BigInteger tmp = 1;
+
+			for (int i = n; i > 0; i--)
+			{
+				tmp *= i;
+			}
+
+			return tmp;
+		}
 	}
 }
